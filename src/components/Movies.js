2946ebcdff476c0d8ae743a93e1c19e0ea1e945f
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-24 12:31:20 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -8,8 +8,8 @@ const Movies = () => {
         <h1> Movies Page </h1>
         {movies.map((movie,index) => (
         <div key={index}>
-        <h4> {movie.name} </h4>
-        <p> {movie.time} </p>
+        <h4>{movie.name}</h4>
+        <p>{movie.time}</p>
         <ul>
           {movie.genres.map((genre,index)=>(<li key={index}>{genre}</li>))}
         </ul>
