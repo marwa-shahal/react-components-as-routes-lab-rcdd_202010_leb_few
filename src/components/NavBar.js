95b1965eaff09ad571d360d1c5eb57cfefda788a
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-24 12:08:17 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,31 @@ const NavBar = () => {
   return (
     <div>
       {/*{code here}*/}
-      
+       <NavLink
+          to="/"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >Home</NavLink>
+        
+        <NavLink
+          to="/directors"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >Directors</NavLink>
+        <NavLink
+          to="/login"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >Login</NavLink>
     </div>
   );
 };
