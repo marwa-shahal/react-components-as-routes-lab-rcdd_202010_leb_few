93d1b35d2268c401869bb1c666b1dc28555a80a8
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-24 12:22:02 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,17 @@ const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page </h1>
+      {directors.map((director)=>(
+      <div> 
+      <h4>{director.name}</h4>
+      <ul>
+      {directors.movies.map((movie)=>(
+        <li>{movie} </li>
+      ))}
+      </ul>
+      </div>
+      ))}
     </div>
   );
 }
