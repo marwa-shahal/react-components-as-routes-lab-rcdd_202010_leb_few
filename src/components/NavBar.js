9c82ccaf0e5d5d4ee02da8323f0bd80b78f54b10
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-24 12:17:37 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+ 
+
 const NavBar = () => {
   return (
     <div>
